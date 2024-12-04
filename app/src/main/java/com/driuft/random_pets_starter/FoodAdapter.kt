4fc3f5f2addd975fdfa24a896764fff744f7311b
Travis-A <arguellotravis@gmail.com>
2024-12-03 20:08:57 -0600

    Final Product

--- app/src/main/java/com/driuft/random_pets_starter/FoodAdapter.kt
+++ app/src/main/java/com/driuft/random_pets_starter/FoodAdapter.kt
@@ -2,42 +2,50 @@ package com.driuft.random_pets_starter
 
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
+import org.json.JSONArray
 
-class PetAdapter (private val petList: List<String>, private val nameList: List<String>) : RecyclerView.Adapter<ViewHolder>() {
-    lateinit var nameView: TextView
+class FoodAdapter (private val foodList: List<String>, private val nameList: List<String>) : RecyclerView.Adapter<ViewHolder>() {
+    lateinit var nameButton: Button
+    private var onClickListener: OnClickListener? = null
+
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        var petImage: ImageView
+        var foodImage: ImageView
+
         // var nameView: TextView
 
         init {
             // Find our RecyclerView item's ImageView for future use
-            petImage = view.findViewById(R.id.pet_image)
+            foodImage = view.findViewById(R.id.pet_image)
             // nameView = view.findViewById(R.id.nameView)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.pet_item, parent, false)
+            .inflate(R.layout.food_item, parent, false)
 
         return ViewHolder(view)
     }
 
-    override fun getItemCount() = petList.size
+    override fun getItemCount() = foodList.size
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        nameView = holder.itemView.findViewById(R.id.nameView)
+        // val item = foodList[position]
+        nameButton = holder.itemView.findViewById(R.id.nameButton)
+
+
         Glide.with(holder.itemView)
-            .load(petList[position])
+            .load(foodList[position])
             .centerCrop()
             .into(holder.itemView.findViewById(R.id.pet_image))
-        nameView.text = nameList[position]
+        nameButton.text = nameList[position]
     }
-
 }
--- app/src/main/java/com/driuft/random_pets_starter/MainActivity.kt
+++ app/src/main/java/com/driuft/random_pets_starter/MainActivity.kt
@@ -5,53 +5,77 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+import android.content.Context
+import android.content.Intent
+import android.widget.ImageView
+import android.widget.ScrollView
+import android.widget.TextView
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var petList: MutableList<String>
-    private lateinit var nameList: MutableList<String> // Test
-    private lateinit var rvPets: RecyclerView
-    private var startNumber : Int = 1
+    private lateinit var favList: MutableList<String>
+    private lateinit var foodList: MutableList<String>
+    private lateinit var nameList: MutableList<String>
+    private lateinit var currentFood: String
+    private lateinit var rvFood: RecyclerView
+    private lateinit var adapter: FoodAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        rvPets = findViewById(R.id.recyclerView)
-        petList = mutableListOf()
+        favList = getArrayListFromSharedPreferences("favoritesList")
+
+        for (i in favList.indices) {
+            Log.d("--Favorite--", favList[i].toString())
+        }
+
+        rvFood = findViewById(R.id.recyclerView)
+        foodList = mutableListOf()
         nameList = mutableListOf() // Test
 
-        //getDogImageURL()
-        getPokemonImageURL(startNumber)
+        getRandomFood()
+
+        // Retrieve the saved ArrayList from SharedPreferences
+        val retrievedArrayList = getArrayListFromSharedPreferences("favoritesList")
 
         val nextButton : Button = findViewById(R.id.nextButton)
         val prevButton : Button = findViewById(R.id.prevButton)
+        val favoriteMenuButton : Button = findViewById(R.id.favorites)
+        val infoText : TextView = findViewById(R.id.infoView)
+        val infoScroll : ScrollView = findViewById(R.id.foodInfoView)
+        infoScroll.visibility = View.INVISIBLE
+
+        val adapter = FoodAdapter(foodList, nameList)
     }
 
-    /*
-    private fun getDogImageURL() {
+
+    private fun getRandomFood() {
+        // For group project
         val client = AsyncHttpClient()
+        foodList.clear()
+        nameList.clear()
 
-        client["https://dog.ceo/api/breeds/image/random/20", object : JsonHttpResponseHandler() {
+        client["https://themealdb.com/api/json/v1/1/random.php", object : JsonHttpResponseHandler() {
             override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
-                Log.d("Dog Success", "$json")
-                val petImageArray = json.jsonObject.getJSONArray("message")
+                Log.d("Food Success", "$json")
+                currentFood = json.jsonObject.getJSONArray("meals").toString()
+                val randFoodArray = json.jsonObject.getJSONArray("meals")
+                foodList.add(randFoodArray.getJSONObject(0).getString("strMealThumb"))
+                nameList.add(randFoodArray.getJSONObject(0).getString("strMeal"))
 
-                for (i in 0 until petImageArray.length()) {
-                    petList.add(petImageArray.getString(i))
-                }
 
-                val adapter = PetAdapter(petList)
-                rvPets.adapter = adapter
-                rvPets.layoutManager = LinearLayoutManager(this@MainActivity)
+
+                adapter = FoodAdapter(foodList, nameList)
+                rvFood.adapter = adapter
+                rvFood.layoutManager = LinearLayoutManager(this@MainActivity)
             }
 
             override fun onFailure(
@@ -60,57 +84,127 @@ class MainActivity : AppCompatActivity() {
                 errorResponse: String,
                 throwable: Throwable?
             ) {
-                Log.d("Dog Error", errorResponse)
+                Log.d("Food Error", errorResponse)
             }
         }]
     }
-    */
 
+    private fun getFavoritesMenu() {
+        foodList.clear()
+        nameList.clear()
 
-    private fun getPokemonImageURL(startNum: Int) {
-        petList.clear()
-        nameList.clear() // Test
-        val client = AsyncHttpClient()
-        val endNum : Int = startNum + 20
-        for (i in startNum until endNum) {
-            client["https://pokeapi.co/api/v2/pokemon/$i/", object : JsonHttpResponseHandler() {
-                override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
-                    Log.d("Pokemon Success", "$json")
-                    Log.d("Pokemon Name", json.jsonObject.getString("name"))
-                    petList.add(json.jsonObject.getJSONObject("sprites").getString("front_default"))
-                    nameList.add(" ID: $i,  " + "name: "  + json.jsonObject.getString("name"))
+        // Update List
+        favList = getArrayListFromSharedPreferences("favoritesList")
 
-                    if (i == endNum - 1) {
-                        val adapter = PetAdapter(petList, nameList)
-                        rvPets.adapter = adapter
-                        rvPets.layoutManager = LinearLayoutManager(this@MainActivity)
-                        Log.d("Pokemon List", petList.toString())
-                    }
-                }
+        for (i in favList) {
+            foodList.add(JSONArray(i).getJSONObject(0).getString("strMealThumb"))
+            nameList.add(JSONArray(i).getJSONObject(0).getString("strMeal"))
+        }
 
-                override fun onFailure(
-                    statusCode: Int,
-                    headers: Headers?,
-                    errorResponse: String,
-                    throwable: Throwable?
-                ) {
-                    Log.d("Pokemon Error", errorResponse)
-                }
-            }]
+        val adapter = FoodAdapter(foodList, nameList)
+        rvFood.adapter = adapter
+        rvFood.layoutManager = LinearLayoutManager(this@MainActivity)
+    }
+
+    fun ignoreFood(view: View?) {
+        getRandomFood()
+        val icon : ImageView = findViewById(R.id.iconView)
+        icon.visibility = View.VISIBLE
+    }
+
+    fun saveFood(view: View?) {
+        if (currentFood.isNotEmpty()) {
+            add_food_to_favorites(currentFood)
+            getRandomFood()
         }
     }
 
-    fun nextTwenty(view: View?) {
-        if (startNumber != 1001) {
-            startNumber += 20
-            getPokemonImageURL(startNumber)
-        }
+    fun favoritesMenu(view: View?) {
+        val icon : ImageView = findViewById(R.id.iconView)
+        icon.visibility = View.INVISIBLE
+        getFavoritesMenu()
+
     }
 
-    fun prevTwenty(view: View?) {
-        if (startNumber != 1) {
-            startNumber -= 20
-            getPokemonImageURL(startNumber)
+    fun exitInfo(view: View?) {
+        var scrollView : ScrollView = findViewById(R.id.foodInfoView)
+        scrollView.visibility = View.INVISIBLE
+    }
+
+    fun showInfo(view: View?) {
+        if (foodList.size == favList.size) {
+            if (view != null) {
+                var position : View? = rvFood.layoutManager?.findContainingItemView(view)
+                if (position != null) {
+                    Log.d("Please work", rvFood.layoutManager?.getPosition(position).toString())
+                    currentFood = favList[rvFood.layoutManager?.getPosition(position)!!]
+                }
+
+            }
+
+            Log.d("Test", foodList.toString())
+            Log.d("TEst2", favList.toString())
+
         }
+        getFoodInfo(JSONArray(currentFood), findViewById(R.id.infoView), findViewById(R.id.foodInfoView))
+    }
+
+    fun add_food_to_favorites(food : String) {
+        favList.add(food)
+        saveArrayListToSharedPreferences("favoritesList", favList)
+    }
+
+    private fun saveArrayListToSharedPreferences(key: String, arrayList: MutableList<String>) {
+        // Convert the ArrayList to a Set of strings
+        val mySet = arrayList.toSet()
+
+        // Get an instance of the SharedPreferences
+        val sharedPreferences = getSharedPreferences("favoritesList", Context.MODE_PRIVATE)
+
+        // Get an instance of the SharedPreferences.Editor
+        val editor = sharedPreferences.edit()
+
+        // Put the Set of strings into the SharedPreferences.Editor
+        editor.putStringSet(key, mySet)
+
+        // Apply the changes to the SharedPreferences
+        editor.apply()
+    }
+
+    private fun getArrayListFromSharedPreferences(key: String): ArrayList<String> {
+        // Get an instance of the SharedPreferences
+        val sharedPreferences = getSharedPreferences("favoritesList", Context.MODE_PRIVATE)
+
+        // Retrieve the Set of strings from SharedPreferences
+        val mySet = sharedPreferences.getStringSet(key, emptySet())
+
+        // Convert the Set of strings back to an ArrayList
+        val myArrayList = arrayListOf<String>()
+        myArrayList.addAll(mySet!!)
+
+        return myArrayList
+    }
+
+    private fun getFoodInfo(food: JSONArray, infoText: TextView, scrollView: ScrollView) {
+        scrollView.visibility = View.VISIBLE
+        var ingredient = 1
+        var info : String = food.getJSONObject(0).getString("strMeal") + "\n"
+        info += "Category: " + food.getJSONObject(0).getString("strCategory") + "\n"
+        info += "Origin: " + food.getJSONObject(0).getString("strArea") + "\n"
+        info += "\nIngredients\n"
+        while (ingredient <= 20) {
+            if (food.getJSONObject(0).getString("strIngredient$ingredient") != "null")
+                if (food.getJSONObject(0).getString("strIngredient$ingredient") != "") {
+                    info += food.getJSONObject(0).getString("strIngredient$ingredient") + " : "
+                    info += food.getJSONObject(0).getString("strMeasure$ingredient") + "\n"
+                }
+            ingredient += 1
+        }
+        info += "\nInstructions\n" + food.getJSONObject(0).getString("strInstructions") + "\n"
+        info += "\nYouTube Video: " + food.getJSONObject(0).getString("strYoutube") + "\n"
+        info += "\nSource: " + food.getJSONObject(0).getString("strSource") + "\n"
+
+        infoText.text = info
+
     }
 }
